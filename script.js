72e97ecd2b0f2ffb88a94cf787fbecc668acdcca
aius5684 <karlickarel@gmail.com>
2025-10-09 19:38:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("September 30 2025 17:00:00").getTime();
+const targetDate = new Date("October 9 2025 18:32:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
